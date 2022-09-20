8de5463650e41c4e0adcbeae31da7a3cb8cf875a
firebat75 <jacklowry1337@gmail.com>
2022-09-20 12:54:29 -0400

    added PollList and styling for current polls

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
 	import Tabs from "./shared/Tabs.svelte";
+	import PollList from "./components/PollList.svelte";
 	import CreateNewPollForm from "./components/CreatePollForm.svelte";
 	import CreatePollForm from "./components/CreatePollForm.svelte";
 
@@ -12,15 +13,50 @@
 	const tabChange = (e) => {
 		activeItem = e.detail;
 	};
+
+	//polls
+	let polls = [
+		{
+			id: 1,
+			question: "Python or Javascript?",
+			answerA: "Python",
+			answerB: "Javascript",
+			votesA: 9,
+			votesB: 15,
+		},
+		{
+			id: 2,
+			question: "Firebase or Pocketbase?",
+			answerA: "Firebase",
+			answerB: "Pocketbase",
+			votesA: 80,
+			votesB: 4,
+		},
+		{
+			id: 3,
+			question: "Do you like Waffles?",
+			answerA: "Yes",
+			answerB: "No",
+			votesA: 4,
+			votesB: 5,
+		},
+	];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = "Current Polls";
+	};
 </script>
 
 <Header />
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange} />
 	{#if activeItem === "Current Polls"}
-		<p>Poll list component goes here</p>
+		<PollList {polls} />
 	{:else if activeItem === "Add New Poll"}
-		<CreatePollForm />
+		<CreatePollForm on:add={handleAdd} />
 	{/if}
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quam
