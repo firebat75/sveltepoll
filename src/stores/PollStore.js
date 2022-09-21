d76d8f1935b7976b31d5e94493ff60a90f767a9e
firebat75 <jacklowry1337@gmail.com>
2022-09-21 14:59:21 -0400

    changed keeping polls in App to keeping polls in PollStore.js

--- src/stores/PollStore.js
+++ src/stores/PollStore.js
@@ -1,2 +1,30 @@
-import { writeable } from "svelte/store";
+import { writable } from "svelte/store";
 
+const PollStore = writable([
+    {
+        id: 1,
+        question: "Python or Javascript?",
+        answerA: "Python",
+        answerB: "Javascript",
+        votesA: 9,
+        votesB: 15,
+    },
+    {
+        id: 2,
+        question: "Firebase or Pocketbase?",
+        answerA: "Firebase",
+        answerB: "Pocketbase",
+        votesA: 80,
+        votesB: 4,
+    },
+    {
+        id: 3,
+        question: "Do you like Waffles?",
+        answerA: "Yes",
+        answerB: "No",
+        votesA: 4,
+        votesB: 5,
+    },
+]);
+
+export default PollStore;
